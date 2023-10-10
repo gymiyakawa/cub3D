559fc760ca4bf2e15d4ca0acd682163e69d6ebea
Gustavo Yuki Miyakawa <gmiyakawa@gmail.com>
2023-10-10 13:10:18 -0400

    added structs.h

---
+++ inc/structs.h
@@ -0,0 +1,59 @@
+
+
+// F 220,100,0
+// C 225,30,0
+typedef struct s_color
+{
+	int	fRed;
+	int	fBlue;
+	int	fGreen;
+	int	cRed;
+	int	cBlue;
+	int	cGreen;
+	int	fColor;
+	int	cColor;
+	
+}	t_color;
+
+
+
+
+typedef struct s_map
+{
+	
+	
+	
+}	t_map;
+
+			// from so_long
+// typedef struct s_map
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	char	**map;
+// 	int		flag;
+// 	int		x;
+// 	int		y;
+// 	int		p_y;
+// 	int		p_x;
+// 	int		e_x;
+// 	int		e_y;
+// 	int		exit_check;
+// 	int		x_max;
+// 	int		y_max;
+// 	void	*img_p;
+// 	void	*img_e;
+// 	void	*img_e2;
+// 	void	*img_w;
+// 	void	*img_c;
+// 	void	*img_z;
+// 	int		img_width;
+// 	int		img_height;
+// 	int		player;
+// 	int		coin;
+// 	int		exit;
+// 	int		moves;
+// 	int		pixel;
+// 	int		pixel_max;
+// 	bool	is_dead;
+// }			t_map;
