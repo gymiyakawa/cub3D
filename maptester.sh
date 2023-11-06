d42b4a19c522f3a59e7ba94fe87fb4c020998727
Gustavo Yuki Miyakawa <gmiyakawa@gmail.com>
2023-11-06 09:39:09 -0500

    updated tester

--- maptester.sh
+++ maptester.sh
@@ -1,8 +1,9 @@
 rm -f log.txt
 for file in ./maps/invalid/*
 do
-	echo "map passed: $file"
 	echo $file >> log.txt
 	leaks --atExit -- ./cub3D $file >> log.txt
+	echo "map passed: $file"
 done
-cat log.txt | grep leaked
+	cat log.txt | grep -A 1 Error
+	cat log.txt | grep leaked
